2e4cf265f24ef58059d1e091b11271ac3a21661d
tj0vtj0v <burdorftjorven@gmail.com>
2024-03-07 23:14:42 +0100

    Licence classifier fixed

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: GNU',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
     ],
